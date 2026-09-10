d6a5193ba861d8787a0e153b7c7298599d9178ee
Rakshitas-14 <rakshitasgowda@gmail.com>
2026-09-10 07:33:10 -0400

    message

--- division.c
+++ division.c
@@ -39,6 +39,7 @@ void division(Node *head1, Node *tail1,
               Node *head2, Node *tail2,
               Node **head_result, Node **tail_result)
 {
+    (void)tail1;
     Node *temp = head1;
 
     Node *temp_head = NULL;
@@ -49,64 +50,61 @@ void division(Node *head1, Node *tail1,
 
     int count;
 
-    /* Check division by zero */
+    /* Division by zero */
     if (head2->data == 0 && head2->next == NULL)
     {
         printf("Division by zero is not possible\n");
         return;
     }
 
-    /* Traverse through T1 */
+    /* Traverse dividend */
     while (temp != NULL)
     {
-        /* Insert H->data into Temph */
+        /* Add current digit to temporary list */
         insert_at_end(&temp_head, &temp_tail, temp->data);
 
+        /* Remove leading zeros */
         remove_leading_zeros(&temp_head, &temp_tail);
 
         count = 0;
 
-        /* Compare Temph and head2 */
-        while (compare_numbers(temp_head, head2) >= 0)
+        /* Subtract divisor repeatedly */
+        while (temp_head != NULL &&
+               compare_numbers(temp_head, head2) >= 0)
         {
-            /* Call subtraction */
             subtraction(temp_tail, tail2,
                         &rem_head, &rem_tail);
 
-            /* Delete list Temph */
+            /* Delete old temporary list */
             free_list(temp_head);
 
-            temp_head = NULL;
-            temp_tail = NULL;
-
-            /* Swap Remh and Temph */
+            /* Swap remainder with temporary list */
             temp_head = rem_head;
             temp_tail = rem_tail;
 
             rem_head = NULL;
             rem_tail = NULL;
 
-            /* Increment Count */
             count++;
         }
 
-        /* Insert Count into Result */
+        /* Store quotient digit */
         insert_at_end(head_result, tail_result, count);
 
-        /* Reset Count */
-        count = 0;
-
-        /* Update temp to next */
+        /* Move to next dividend digit */
         temp = temp->next;
     }
 
-    /* Remove leading zeros */
+    /* Remove leading zeros from quotient */
     remove_leading_zeros(head_result, tail_result);
 
-    /* If result is empty */
+    /* If quotient is empty, result is zero */
     if (*head_result == NULL)
+    {
         insert_at_end(head_result, tail_result, 0);
+    }
 
+    /* Free temporary lists */
     free_list(temp_head);
     free_list(rem_head);
 }
